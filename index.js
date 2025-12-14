7c5c68ea19b01a08c5b66e8868a6663495d6431a
Mitchelle-git <mitchmarios2@gmail.com>
2025-12-14 18:07:12 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // Write your solution in this file
-const Burgers =[`hamburger`,`cheeseburger`];
-let featureDrink=[`strawberryMilkshake`];
+const Burgers =["Hamburger","Cheeseburger"];
+let featureDrink=["Strawberry Milkshake"];
 
 function addBurger(){
-    const newBurger=`flatBurger`;
+    const newBurger="Flatburger";
     Burgers.push(newBurger);
 }
 
 addBurger();
 console.log(Burgers);
 
-if (true){
-    const anotherNewBurger = 'Maple Bacon Burger';
+if(true){
+    const anotherNewBurger = "Maple Bacon Burger";
     Burgers.push(anotherNewBurger);
 }
 
 function changeFeaturedDrink(){
- featureDrink='The JavaShake';
+ featureDrink="The JavaShake";
 }
 
-addBurger();
-changeFeaturedDrink();
 
-console.log (Burgers);
-console.log (featureDrink);
+changeFeaturedDrink();
+console.log(featureDrink)
+
+
 
 
